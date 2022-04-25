694d9d9e9e03d8b92f7553479e3321b79ac6c862
Estiven Mayhuay <developerwebhairton@gmail.com>
2022-04-25 16:44:17 -0500

    delete the storage in the menu hamburger

--- docs/script/modules/menuHamburger.js
+++ docs/script/modules/menuHamburger.js
@@ -1,26 +1,26 @@
-import { getSidesPanel, storeCurrentSidePanel } from "./storageApp.js";
-
 const menuHambuerger = () => {
   const d = document;
-  
+
   d.addEventListener("click", (e) => {
     // show and hide the main menu
-    if(e.target.matches("#buttonMenu") || e.target.matches("#buttonMenu *")) {
-      const hMenu = d.querySelector('.menu').offsetHeight;
-      
-      d.querySelector('#buttonMenu').classList.toggle('activeMenu');
-      d.querySelector('.contentMenu').classList.toggle('activeContentMenu');
+    if (e.target.matches("#buttonMenu") || e.target.matches("#buttonMenu *")) {
+      const hMenu = d.querySelector(".menu").offsetHeight;
 
-      d.querySelector('.contentMenu').style.transition = 'height 0.4s ease';
+      d.querySelector("#buttonMenu").classList.toggle("activeMenu");
+      d.querySelector(".contentMenu").classList.toggle("activeContentMenu");
 
-      !d.querySelector('.contentMenu').style.height || d.querySelector('.contentMenu').style.height == "0px" ? d.querySelector('.contentMenu').style.height = hMenu + 32 + 'px' : d.querySelector('.contentMenu').style.height = 0
+      d.querySelector(".contentMenu").style.transition = "height 0.4s ease";
+
+      !d.querySelector(".contentMenu").style.height ||
+      d.querySelector(".contentMenu").style.height == "0px"
+        ? (d.querySelector(".contentMenu").style.height = hMenu + 32 + "px")
+        : (d.querySelector(".contentMenu").style.height = 0);
+    } else if (!e.target.matches(".contentMenu")) {
+      d.querySelector("#buttonMenu").classList.remove("activeMenu");
+      d.querySelector(".contentMenu").classList.remove("activeContentMenu");
+      d.querySelector(".contentMenu").style.height = 0;
     }
-    else if (!e.target.matches('.contentMenu')) {
-      d.querySelector('#buttonMenu').classList.remove('activeMenu');
-      d.querySelector('.contentMenu').classList.remove('activeContentMenu');
-      d.querySelector('.contentMenu').style.height = 0;
-    }
-  })
-}
+  });
+};
 
-export {menuHambuerger}
+export { menuHambuerger };
