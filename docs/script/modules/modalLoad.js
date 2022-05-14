8fe14d1f149e06b1a345e9c1ceefe23e7e84c4ce
Estiven Mayhuay <developerwebhairton@gmail.com>
2022-05-13 19:34:26 -0500

    add the modal pug, less and js

---
+++ docs/script/modules/modalLoad.js
@@ -0,0 +1,8 @@
+const modalLoad = () => {
+  setTimeout(() => {
+    document.getElementById("modalLoadInfo").style.display = "none";
+    console.log("hola");
+  }, 4000);
+};
+
+export { modalLoad };
